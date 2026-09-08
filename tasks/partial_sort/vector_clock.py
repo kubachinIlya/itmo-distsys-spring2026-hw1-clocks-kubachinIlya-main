0f4e810480abf8c695c92ba02caef294d7e6c21d
kubashinia <kubashinia@NB-0275>
2026-09-08 17:00:10 +0300

    initial commit for hw1

---
+++ tasks/partial_sort/vector_clock.py
@@ -0,0 +1,7 @@
+from typing import Dict
+
+VectorClock = Dict[str, int]
+
+def partial_sort(timestamps: list[VectorClock]) -> list[VectorClock]:
+    # TODO: implement me (task 2)
+    return timestamps
---
+++ tasks/time_synchronization/cristian.py
@@ -0,0 +1,9 @@
+from tasks.time_synchronization.clock import Clock
+
+"""
+Implement Cristian's algorithm for clock synchronization here.
+Use only public interface of the Clock class.
+"""
+def cristian_time_synchronize(local_clock: Clock, remote_clock: Clock) -> None:
+    # TODO: implement me (task 1.1)
+    pass
